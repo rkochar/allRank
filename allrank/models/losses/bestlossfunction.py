8c5a89f9f62cf0bc89832739a6b23efec8054ea0
Rahul Kochar <rkochar9@gmail.com>
2023-03-25 16:15:00 +0100

    best loss function

--- allrank/models/losses/bestlossfunction.py
+++ allrank/models/losses/bestlossfunction.py
@@ -1,9 +1,12 @@
 import torch
 
 from allrank.data.dataset_loading import PADDED_Y_VALUE
-from torch.nn import KLDivLoss, HingeEmbeddingLoss, CrossEntropyLoss, BCEWithLogitsLoss, MarginRankingLoss
+from torch.nn import KLDivLoss, HingeEmbeddingLoss, CrossEntropyLoss, BCEWithLogitsLoss, MarginRankingLoss, HingeEmbeddingLoss, NLLLoss2d, MSELoss, L1Loss
 from torch import rand
-from rankNet import rankNet
+from allrank.models.losses.rankNet import rankNet
+from allrank.models.losses.neuralNDCG import neuralNDCG
+from allrank.models.losses.listMLE import listMLE
+from allrank.models.losses.approxNDCG import approxNDCGLoss
 
 def bestlossfunction(y_pred, y_true, padded_value_indicator=PADDED_Y_VALUE):
     """
@@ -22,20 +25,36 @@ def bestlossfunction(y_pred, y_true, padded_value_indicator=PADDED_Y_VALUE):
     y_true[mask] = 0
     y_pred[mask] = 0
 
-    crossloss = CrossEntropyLoss()
-    cl = crossloss(y_true, y_pred)
+    # crossloss = CrossEntropyLoss()
+    # cl = crossloss(y_true, y_pred)
+    # print("cl: " + str(cl))
 
     leiblerloss = KLDivLoss()
     ll = leiblerloss(y_true, y_pred)
+    # print("ll: " + str(ll))
 
-    rn = rankNet(y_pred, y_true)
+    # rn = rankNet(y_pred, y_true)
+    nndcg = neuralNDCG(y_pred, y_true)
+    # andcg = approxNDCGLoss(y_pred, y_true)
+
+    # mle = listMLE(y_true, y_pred)
 
     # marginrankingloss = MarginRankingLoss()
     # mrl = marginrankingloss(y_true, y_pred)
-    # print(mrl)
+    # grad
 
-    return (ll + cl + rn) / 3
+    # hinge = HingeEmbeddingLoss()
+    # hingeloss = hinge(y_true, y_pred)
+    # grad
 
+    # nll = NLLLoss2d()
+    # nl = nll(y_true, y_pred)
+    # dim
 
-def get_tensor_value(t):
-    return float(t[t.find("(")+1 : t.find(",")])
+    #mseloss = MSELoss()
+    #mse = mseloss(y_true, y_pred)
+
+    L1loss = L1Loss()
+    l1 = L1loss(y_true, y_pred)
+
+    return ll + 3 * l1 + nndcg
--- scripts/run_example.sh
+++ scripts/run_example.sh
@@ -2,6 +2,8 @@
 
 DIR=$(dirname $0)
 PROJECT_DIR="$(cd $DIR/..; pwd)"
+RUNID=$1
+echo "[RUNID]: $RUNID"
 
-docker build -t allrank:latest $PROJECT_DIR
-docker run -e PYTHONPATH=/allrank -v $PROJECT_DIR:/allrank allrank:latest /bin/sh -c 'python allrank/main.py --config-file-name /allrank/scripts/custom_config.json --run-id test_run --job-dir /output/allrank/bestlossfunction'
+#docker build -t allrank:latest $PROJECT_DIR
+docker run -e PYTHONPATH=/allrank -v $PROJECT_DIR:/allrank allrank:latest /bin/sh -c 'python allrank/main.py --config-file-name /allrank/scripts/custom_config.json --run-id bestlossfunction --job-dir /output/bestlossfunction/$RUNID'
